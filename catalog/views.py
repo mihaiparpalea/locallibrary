88504cbcf8b0d2203c03bb7b5aa646a4144f46ea
Mihai Parpalea <mihai.parpalea@gmail.com>
2023-05-25 10:07:51 +0300

    First commit.

---
+++ catalog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+	"""View function for home page of site."""
+
+	# Generate counts of some of the main objects
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+
+	# Available books (status = 'a')
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+	# The 'all()' is implied by default.
+	num_authors = Author.objects.count()
+	num_genres = Genre.objects.count()
+	num_word = Book.objects.filter(title__icontains='abație').count()
+
+	context = {
+		'num_books': num_books,
+		'num_instances': num_instances,
+		'num_instances_available': num_instances_available,
+		'num_authors': num_authors,
+		'num_genres': num_genres,
+		'num_word': num_word,
+	}
+
+	# Render the HTML template index.html with the data in the context variable
+	return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+	model = Book
+	paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+	model = Book
+
+class AuthorListView(generic.ListView):
+	model = Author
+	paginate_by = 3
+
+class AuthorDetailView(generic.DetailView):
+	model = Author
